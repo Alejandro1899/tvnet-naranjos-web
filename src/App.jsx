d17c0318376222de2dd3d192a9c1ead446207ed2
Luis Joel Alejandro Cristino Arteaga <ale.arteaga99@outlook.com>
2025-03-14 17:19:43 -0600

    cambio en src

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route  } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Channels from './components/Channels';
 import PricePlans from './components/PricePlans';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-import  NavigationBar  from './components/NavigationBar';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <>
-    <NavigationBar/>
+      <NavigationBar />
       <Routes>
         <Route path="/" >
-          <Route path="/Home" index element={<Home />} />
-          <Route path="/Channels" element={<Channels />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/PricePlans" element={<PricePlans />} />
-          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/home" index element={<Home />} />
+          <Route path="/channels" element={<Channels />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/price-plans" element={<PricePlans />} />
+          <Route path="/contact-us" element={<ContactUs />} />
         </Route>
       </Routes>
 
